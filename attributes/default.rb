13747cbc1deb585acc73df3c928a615d60a1d2df
Richard Lister <ric@burton-5.home>
2014-12-13 18:40:24 -0500

    config file template

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,8 @@
-default[:hastebin][:user] = 'root'
-default[:hastebin][:group] = 'root'
+default[:hastebin][:user]    = 'root'
+default[:hastebin][:group]   = 'root'
+default[:hastebin][:host]    = '0.0.0.0'
+default[:hastebin][:port]    = 7777
+default[:hastebin][:storage] = {
+  path: './data',
+  type: 'file'
+}
